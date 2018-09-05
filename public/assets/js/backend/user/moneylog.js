5887324b81769e6e89b3471676f39abc446b1bbf
Dong <435795@qq.com>
2018-09-06 01:46:43 +0800

    1:基础插件的安装 2:用户基础数据库的搭建

---
+++ public/assets/js/backend/user/moneylog.js
@@ -0,0 +1,60 @@
+define(['jquery', 'bootstrap', 'backend', 'table', 'form'], function ($, undefined, Backend, Table, Form) {
+
+    var Controller = {
+        index: function () {
+            // 初始化表格参数配置
+            Table.api.init({
+                extend: {
+                    index_url: 'user/moneylog/index',
+                    add_url: 'user/moneylog/add',
+                    edit_url: 'user/moneylog/edit',
+                    del_url: 'user/moneylog/del',
+                    multi_url: 'user/moneylog/multi',
+                    table: 'user_money_log',
+                }
+            });
+
+            var table = $("#table");
+
+            // 初始化表格
+            table.bootstrapTable({
+                url: $.fn.bootstrapTable.defaults.extend.index_url,
+                pk: 'id',
+                sortName: 'id',
+                columns: [
+                    [
+                        {checkbox: true},
+                        {field: 'id', title: __('Id')},
+                        {field: 'user_id', title: __('User_id')},
+                        {field: 'user.mobile', title: __('User.mobile')},
+                        {field: 'money', title: __('Money')},
+                        {field: 'type', title: __('Type')},
+                        {field: 'flow', title: __('Flow'), searchList: {"add":__('Add'),"dec":__('Dec')}, formatter: Table.api.formatter.normal},
+                        {field: 'before', title: __('Before')},
+                        {field: 'after', title: __('After')},
+                        {field: 'memo', title: __('Memo')},
+                        {field: 'createtime', title: __('Createtime'), operate:'RANGE', addclass:'datetimerange', formatter: Table.api.formatter.datetime},
+                        {field: 'operate', title: __('Operate'), table: table, events: Table.api.events.operate, formatter: Table.api.formatter.operate}
+                    ]
+                ]
+            });
+
+            
+
+            // 为表格绑定事件
+            Table.api.bindevent(table);
+        },
+        add: function () {
+            Controller.api.bindevent();
+        },
+        edit: function () {
+            Controller.api.bindevent();
+        },
+        api: {
+            bindevent: function () {
+                Form.api.bindevent($("form[role=form]"));
+            }
+        }
+    };
+    return Controller;
+});
